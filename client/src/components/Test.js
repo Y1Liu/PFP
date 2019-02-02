c4c255eb3d2919cdebc8037b61cc78c5288c3489
Y1Liu <34066088+Y1Liu@users.noreply.github.com>
2019-02-02 23:06:37 +0100

    point of interest

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,41 +1,72 @@
-import React, { Component } from 'react'
-import ReactDom from 'react-dom';
-import axios from 'axios'
+/*global google*/
+import React from "react"
+import { compose, withProps, withHandlers, withState } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-class Nav extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            arr: {}
-        };
-        this.get = this.get.bind(this);
-    }
-    get(){
-        axios.get('/test').then((res)=>{
-            console.log(res.data);
+var Paris = { lat: 48.86, lng: 2.33 }
 
-            for (var i = 0; i <=res.data.length-1; i++) {
-                console.log(res.data[i][0])
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyB8pxsl2jFQSwshMT2I5Weue8CKLgxalY8&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `90vh` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+  }),
+    withScriptjs,
+    withGoogleMap,
+    withState('places', 'updatePlaces', ''),
+    withHandlers(() => {
+        const refs = {
+            map: undefined,
+        }
+
+        return {
+            onMapMounted: () => ref => {
+                refs.map = ref
+            },
+            fetchPlaces: ({ updatePlaces }) => {
+                let places;
+                const bounds = refs.map.getBounds();
+                const service = new google.maps.places.PlacesService(refs.map.context.
+                __SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+                const request = {
+                    bounds: bounds,
+                    type: ['point of interest']
+                };
+                service.nearbySearch(request, (results, status) => {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        console.log(results);
+                        updatePlaces(results);
+                    }
+                })
             }
-            
-            
-        }).catch((err)=>{
-            console.log(err.status);
-        })
-    }
-    render(){
-        return <div>
-            <input type="button" value="Test" onClick={this.get}/>
-       
+        }
+    }),
+)((props) => {
+    return ( 
+    <div>
 
-        </div>
+        <GoogleMap
+            onTilesLoaded={props.fetchPlaces}
+            ref={props.onMapMounted}
+            onBoundsChanged={props.fetchPlaces}
+            defaultZoom={16}
+            defaultCenter={Paris}
+        >
+            {props.places && props.places.map((place, i) =>
+                <Marker key={i} position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }} />
+            )}
+        </GoogleMap>
+    </div>
+    )
+})
 
-        
+export default class MyFancyComponent extends React.PureComponent {
+    render() {
+        return (
+           <div>
+            <MyMapComponent />
+           </div>
+        )
     }
 }
-
-
-
-
-
-export default Nav
