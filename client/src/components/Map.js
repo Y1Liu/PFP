f36c5a5a278ca84860a8fd22f00ebd3fe8d51fe0
Y1Liu <34066088+Y1Liu@users.noreply.github.com>
2019-02-03 20:54:36 +0100

    nav2

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -2,6 +2,10 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom';
 import axios from 'axios'
+import { compose, withProps, withHandlers, withState } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+var Paris = { lat: 48.86, lng: 2.33 }
 
 
 
@@ -14,6 +18,61 @@ import axios from 'axios'
 
 
 
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyB8pxsl2jFQSwshMT2I5Weue8CKLgxalY8&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `80vh` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+  }),
+    withScriptjs,
+    withGoogleMap,
+    withState('places', 'updatePlaces', ''),
+    withHandlers(() => {
+        const refs = {
+            map: undefined,
+        }
+
+        return {
+            onMapMounted: () => ref => {
+                refs.map = ref
+            },
+            fetchPlaces: ({ updatePlaces }) => {
+                let places;
+                const bounds = refs.map.getBounds();
+                const service = new google.maps.places.PlacesService(refs.map.context.
+                __SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+                const request = {
+                    bounds: bounds,
+                    type: ['point of interest']
+                };
+                service.nearbySearch(request, (results, status) => {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        console.log(results);
+                        updatePlaces(results);
+                    }
+                })
+            }
+        }
+    }),
+)((props) => {
+    return ( 
+    <div>
+
+        <GoogleMap
+            onTilesLoaded={props.fetchPlaces}
+            ref={props.onMapMounted}
+            onBoundsChanged={props.fetchPlaces}
+            defaultZoom={16}
+            defaultCenter={Paris}
+        >
+            {props.places && props.places.map((place, i) =>
+                <Marker key={i} position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }} />
+            )}
+        </GoogleMap>
+    </div>
+    )
+})
 
 
 class Map extends React.PureComponent {
@@ -69,8 +128,6 @@ class Map extends React.PureComponent {
     }
 
   render() {
-
-
 const { compose, withProps, lifecycle } = require("recompose");
 const {
   withScriptjs,
@@ -86,7 +143,7 @@ const MapWithADirectionsRenderer = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyB8pxsl2jFQSwshMT2I5Weue8CKLgxalY8&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100vh` }} />,
+    containerElement: <div style={{ height: `80vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
@@ -134,20 +191,20 @@ const MapWithADirectionsRenderer = compose(
 
     return (
 <div class="container">
-
-  <ul class="nav nav-tabs" role="tablist">
+   <br></br>
+   <ul class="nav nav-tabs" role="tablist">
     <li class="nav-item">
-      <a class="nav-link active" data-toggle="tab" href="#home" onClick={this.get}>Home</a>
+      <a class="nav-link active" data-toggle="tab" href="#home" onClick={this.get}>Show your trip on the map</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#menu1">Menu 1</a>
+      <a class="nav-link" data-toggle="tab" href="#menu1">Discory your destination</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
+      <a class="nav-link" data-toggle="tab" href="#menu2">Your schedule</a>
     </li>
-  </ul>
-
-  <div class="tab-content">
+    </ul>
+    <br></br>
+    <div class="tab-content">
     <div id="home" class="container tab-pane active">
        <MapWithADirectionsRenderer
         isMarkerShown={this.state.isMarkerShown}
@@ -155,15 +212,14 @@ const MapWithADirectionsRenderer = compose(
       />
     </div>
     <div id="menu1" class="container tab-pane fade">
-      <h3>Menu 1</h3>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <MyMapComponent />
     </div>
     <div id="menu2" class="container tab-pane fade">
       <h3>Menu 2</h3>
       <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
     </div>
+    </div>
   </div>
-</div>
 
 
     
